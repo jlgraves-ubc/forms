4c5eb474f38f3a087f1381cc20ac37d44f42d14d
Jonathan Graves <65632825+jlgraves-ubc@users.noreply.github.com>
2022-11-05 13:45:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Forms Extension For Quarto
+
+This extension allows you to add an HTML form to your Quarto HTML pages, using the the `form` shortcode
+
+
+
+## Installing
+
+```bash
+quarto add jlgraves-ubc/forms
+```
+This will install the extension under the `_extensions` subdirectory.
+If you're using version control, you will want to check in this directory.
+
+## Using
+
+The forms extension has two parts:
+
+1) A set of metadata, in the document header, which defines the form's part
+2) A shortcode `{{< form >}}` which embeds it in the appropriate place in the document.
+
+## Form Definition
+
+You define a form using the metadata in the document header, as follows:
+
+```yaml
+---
+form:
+  action: "/action.js"
+  submit: "Submit Now!"
+  fields:
+  - name: "A Text Field"
+  - type: "text"
+  - id: "textfield1" 
+---
+```
+
+Fields which take multiple entry values take a `values` parameter, which looks like:
+
+```yaml
+  - name: Checkbox
+    type: checkbox
+    id: checkbox1
+    label: "My Checkbox"
+    values:
+    - text: "High"
+      value: "hi"
+    - text: "Low"
+      value: "lo"
+    - text: "Mid"
+      value: "mid"
+```
+
+You can see more detailed (and better-formatted) documentation in the example, below.
+
+## Example
+
+Here is the source code for a minimal example: [example.qmd](example.qmd).
+
---
+++ _extensions/forms/forms.lua
@@ -0,0 +1,150 @@
+-- Forms.lua
+-- Author: Jonathan Graves
+
+-- Helper Functions
+
+local function checkHTMLDeps()
+  -- check the HTML requirements for the library used
+end
+
+local function isEmpty(s)
+  return s == '' or s == nil
+end
+
+-- Main Form Function Shortcode
+
+return {
+
+["form"] = function(args, kwargs, meta)
+
+
+  if quarto.doc.is_format("html:js") then
+    -- this will only run for HTML documents
+    checkHTMLDeps()
+
+    local submit_text = pandoc.utils.stringify(meta["form.submit"])
+
+    -- These are the form items
+    -- action (form.action) describes the handler function for the form submit
+    local form_start = "<div id = \"form\" class = \"form-wrapper\">\n <form action = \"" .. pandoc.utils.stringify(meta["form.action"]) .. "\">\n"
+    local form_end = "</form></div>\n"
+
+    -- Fields for the Form --
+    local fields = meta["form.fields"]
+
+    for f = 1, #fields do
+      
+      local field = fields[f]
+
+
+      if isEmpty(field.type) and not isEmpty(field.text) then
+      -- Handle non-fields (i.e. text)
+        
+        text = pandoc.utils.stringify(field.text)
+
+        if text == "—" then
+          form_start = form_start .. "<hr>" .. "\n"
+        else
+          form_start = form_start .. "<br><p>" .. text .. "</p>\n"
+        end
+      else 
+      -- Handle regular form fields
+
+        local name = pandoc.utils.stringify(field.name)
+        local type = pandoc.utils.stringify(field.type)
+        local id = pandoc.utils.stringify(field.id)
+        local label = pandoc.utils.stringify(field.label)
+
+        if type == "text" then
+        -- Handle text fields
+
+          form_start = form_start .. "<label for=\"" .. id .. "\">" .. label .. "</label><br>\n"
+          form_start = form_start .. "  <input type = \"text\" id = \"" .. id .. "\" name = \"" .. name .. "\"><br>\n"
+
+        elseif type == "textarea" then
+        -- Handle textarea fields
+          local cols = "30"
+          local rows = "10"
+
+          if not isEmpty(field.width) then
+              cols = pandoc.utils.stringify(field.width)
+          end
+
+          if not isEmpty(field.height) then
+            rows = pandoc.utils.stringify(field.height)
+          end
+              
+            
+
+          form_start = form_start .. "<label for =\"" .. id .. "\">" .. label .. "</label><br>\n"
+          form_start = form_start .. "<textarea name =\"" .. name .. "\" id = \"" .. id .. "\" rows = ..\"" .. rows .. "\" cols =\"" .. cols .."\"></textarea><br>\n"
+
+        elseif type == "select" then
+        -- Handle selector (drop-dpwns)
+          
+          local size = "3" 
+          if not isEmpty(field.size) then
+            size =  pandoc.utils.stringify(field.size)
+          end
+
+
+          local multiple = "" 
+          if not isEmpty(field.multiple) then
+            if pandoc.utils.stringify(field.multiple) == "true" then
+              multiple = "multiple"
+            end
+          end
+
+          form_start = form_start .. "<label for = \"" .. id .. "\">" .. label .. "</label><br>\n"
+          form_start = form_start .. "<select id= \"" .. id .. "\" size = \"" .. size .. "\" " .. multiple .. ">\n"
+          
+          for v = 1,#field.values do
+            local value = field.values[v]
+            local label_t = pandoc.utils.stringify(value.text)
+            local val = pandoc.utils.stringify(value.value)
+
+            form_start = form_start .. "  <option value = \"" .. val .. "\">" .. label_t .. "</option><br>\n"
+            
+          end
+
+          form_start = form_start .. "</select><br>\n"
+            
+        else
+        -- Handle radio and checkboxes
+
+          form_start = form_start .. "<span>" .. label .. "</span><br>\n"
+
+          for v = 1, #field.values do
+
+            local value = field.values[v]
+            -- multi-option fields need to have a label for each value
+            local label_t = pandoc.utils.stringify(value.text)
+            local val = pandoc.utils.stringify(value.value)
+
+            
+            form_start = form_start .. "  <input type = \"" .. type .. "\" id = \"" .. id .. "\" name = \"" .. name .. "\" value =\"" .. val .. "\">\n"
+            form_start = form_start .. "  <label for=\"" .. id .. "\">" .. label_t .. "</label><br> \n"
+            -- <input type = "type" id = "id" name = "name" value = "label_v">
+            -- <label for = "id">label_t</label><br>
+          end
+
+
+        end
+
+      end
+    end
+    -- Close the form and submit
+    form_start = form_start .. "<input type=\"submit\" value = \"" .. submit_text .. "\"> \n"
+    form_start = form_start .. form_end
+    --quarto.log.output(form_start)
+    return pandoc.RawInline("html", form_start)
+
+
+  else
+    return pandoc.Null()
+  end
+
+end
+
+}
+
